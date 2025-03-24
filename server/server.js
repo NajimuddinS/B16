afbca891a07fe04b43d4e4cc4b90371d150270c5
Najimuddin Shaikh <sherlockshaikh@gmail.com>
2025-03-24 09:41:41 +0530

    files added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path')
 
 // Initialize Express
 const app = express();
